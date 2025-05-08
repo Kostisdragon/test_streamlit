fa0a311178516e8a7fc7e832a73a86296a184f47
Konstantinos Drakonakis <kostisdragon@hotmail.com>
2025-05-08 15:20:56 +0200

    update: άλλαξα το περιεχόμενο, μήνυμα προς Γιάννη

--- test_streamlit.py
+++ test_streamlit.py
@@ -2,8 +2,13 @@ import pandas as pd
 import streamlit as st
 
 st.title("Test Streamlit")
-st.write("Μαρίζα είσαι όμορφη!!")
-
 st.write("Έφτιαξα την πρώτη μου σελίδα για στατιστικά δεδομένα μέσω Python!!")
+
+st.write("Γιάννη τι κάμνεις;!")
+st.write("Πρέπει να φύγω για δουλειά. Να τα πούμε πιο αργά; Σχολάω 21:30")
+st.write("Απάντα μου στο messenger, είναι πιο απλό")
+
+
+
 
 
